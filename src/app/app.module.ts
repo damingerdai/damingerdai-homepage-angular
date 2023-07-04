3a0f44661a65234794994b66a22c4921e617c041
Arthur Ming <mingguobin@live.com>
2023-07-04 17:23:26 +0800

    refactor: add layout componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BlogLinkComponent } from './blog-link/blog-link.component';
 import { DividerComponent } from './divider/divider.component';
 import { HeaderComponent } from './header/header.component';
 import { LanguageAndToolsComponent } from './language-and-tools/language-and-tools.component';
+import { LayoutComponent } from './layout/layout.component';
 import { MyGithubStatsComponent } from './my-github-stats/my-github-stats.component';
 import { MyMostUsedLanguagesComponent } from './my-most-used-languages/my-most-used-languages.component';
 import { MyOpenSourceComponent } from './my-open-source/my-open-source.component';
@@ -22,6 +23,7 @@ const standaloneComponents = [
   DividerComponent,
   HeaderComponent,
   LanguageAndToolsComponent,
+  LayoutComponent,
   MyGithubStatsComponent,
   MyMostUsedLanguagesComponent,
   MyOpenSourceComponent,
